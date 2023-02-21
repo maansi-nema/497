c0949d5950ea19afb693a38e81f9c46802a160b8
mnema <mnema@umich.edu>
2023-02-21 15:21:37 -0500

    first commit

---
+++ website.js
@@ -0,0 +1,12 @@
+// password manager functionality
+function savePassword() {
+    // get input values and store them in local storage
+  }
+  
+  function getPassword() {
+    // retrieve password from local storage and display in table
+  }
+  
+  function deletePassword() {
+    // remove password
+  }  
